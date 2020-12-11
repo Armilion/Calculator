5213dee50058241bb76b01f138da8fdd83b0faa4
Armilion <valentin.andre94@gmail.com>
2020-12-11 19:37:37 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Display from "./Display";
+import Clear from "./Clear";
+import ClearEntry from "./ClearEntry";
+import ClearOne from "./ClearOne";
+import Parenthesis from "./Parenthesis";
+import NumberComp from "./NumberComp";
+import Operator from "./Operator";
+import Decimal from "./Decimal";
+import Compute from "./Compute";
+import {useState, useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const numbersArray = [{nb: "0", nbStr:"zero"}, {nb: "1", nbStr:"one"} ,{nb: "2", nbStr:"two"} ,{nb: "3", nbStr:"three"} ,{nb: "4", nbStr:"four"} ,{nb: "5", nbStr:"five"} ,{nb: "6", nbStr:"six"}, {nb: "7", nbStr:"seven"}, {nb: "8", nbStr:"eight"}, {nb: "9", nbStr:"nine"}];
+const operatorsArray = [{opSign:"/", opName:"divide"}, {opSign:"*", opName:"multiply"}, {opSign:"+", opName:"add"}, {opSign:"-", opName:"subtract"}];
+
+const Calculator = () => {
+  const [currentOp, setCurrentOp] = useState("");
+  const [expression, setExpression] = useState("0");
+  const [result, setResult] = useState("");
+  const [currentNumber, setCurrentNumber] = useState("");
+  
+  useEffect(() => {
+    let lastNumber = expression.match(/\d+([.]{1}\d*)?$/g);
+    if(lastNumber === null)
+      lastNumber = "";
+    else{
+      lastNumber = expression.match(/\d+([.]{1}\d*)?$/g)[0];
+      console.log(lastNumber);
+    }
+    setCurrentNumber(lastNumber);
+  }, [expression]);
+  
+  return(
+    <div id="calculator">
+      <Display expression={expression} result={result}/>
+      <Clear setExpression={setExpression} setResult={setResult}/>
+      <ClearEntry expression={expression} setExpression={setExpression} currentNumber={currentNumber} setCurrentNumber={setCurrentNumber}/>
+      <ClearOne expression={expression} setExpression={setExpression}/>
+      <Parenthesis id="parenthesisLeft" side="(" expression={expression} setExpression={setExpression}/>
+      <Parenthesis id="parenthesisRight" side=")" expression={expression} setExpression={setExpression}/>
+      {numbersArray.map((numb) => <NumberComp key={numb.nb} number={numb.nb} id={numb.nbStr} expression={expression} setExpression={setExpression} currentNumber={currentNumber}/>)}
+      
+      {operatorsArray.map((operator) => <Operator key={operator.opName} operator={operator.opSign} id={operator.opName} expression={expression} setExpression={setExpression} result={result}/>)}
+      <Decimal expression={expression} setExpression={setExpression} currentNumber={currentNumber}/>
+      <Compute expression={expression} setExpression={setExpression} setResult={setResult}/>
     </div>
   );
 }
 
-export default App;
+export default Calculator;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Calculator from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('full-page')
 );
 
 // If you want to start measuring performance in your app, pass a function
